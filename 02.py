5049efcbcc1a4a2ce4720f302ec256f360bf36f2
Parthiv Dhameliya <parthivdhameliya3@gmail.com>
2024-10-22 22:44:17 -0600

    2nd file created

---
+++ 02.py
@@ -0,0 +1,18 @@
+# 01
+# break = 5
+
+# 02
+birthYear = 2003
+currentYear = 2024
+
+print(currentYear - birthYear)
+
+# 03
+first = 'Parthiv'
+middle = 'Vipulbhai'
+last = 'Dhameliya'
+
+print('My full name is:', first, middle, last)
+
+#4
+# 1record, record-one, record^one, continue
